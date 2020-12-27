dd5db18677c0f70b46c1a59f8521615b2eb91f49
Blue <blue@nqn.blue>
2020-12-27 15:53:12 +0000

    Add alias related commands

--- redis_helper/emote_transform/__init__.py
+++ redis_helper/emote_transform/__init__.py
@@ -1,1 +1,2 @@
 from . import user_cache
+from . import alias
---
+++ redis_helper/emote_transform/alias.py
@@ -0,0 +1,31 @@
+from typing import Dict, NoReturn
+from aioredis import Redis
+
+
+async def assign(redis: Redis, user: int, name: str, value: str) -> NoReturn:
+    tr = redis.pipeline()
+    tr.hset(f"aliases-{user}", name, value)
+    tr.sadd("aliases", user)
+    await tr.execute()
+
+
+async def fetch(redis: Redis, user: int, name: str) -> str:
+    return await redis.hget(f"aliases-{user}", name)
+
+
+async def list(redis: Redis, user: int) -> Dict[str, str]:
+    return await redis.hgetall(f"aliases-{user}")
+
+
+async def delete(redis: Redis, user: int, name: str) -> NoReturn:
+    tr = redis.pipeline()
+    tr.hdel(f"aliases-{user}", name)
+    alias_count = tr.hlen(f"aliases-{user}")
+    await tr.execute()
+    alias_count = await alias_count
+    if alias_count == 0:
+        await redis.srem("aliases", user)
+
+
+async def count(redis: Redis, user: int) -> int:
+    return await redis.hlen(f"aliases-{user}")
