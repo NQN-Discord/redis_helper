a858849478d960bfd774f990b7bd738852c529ef
Blue <blue@nqn.blue>
2021-02-26 19:49:15 +0000

    Maybe this time we fixed TTL

--- redis_helper/ignored_user.py
+++ redis_helper/ignored_user.py
@@ -1,5 +1,6 @@
 from typing import NoReturn, Tuple
 from aioredis import Redis
+from contextlib import asynccontextmanager
 
 DM_SPAM_RATELIMIT = 24 * 60 * 60
 RATELIMIT_TIME = 5 * 60
@@ -21,27 +22,31 @@ async def delete(redis: Redis, user: int) -> NoReturn:
     await redis.srem("message-repost-ignored-users", user)
 
 
-async def total_and_unique_messages(redis: Redis, user: int, content: str) -> Tuple[int, int, bool]:
+@asynccontextmanager
+async def _ttl(redis, user):
     tr = redis.pipeline()
     count_ttl = tr.ttl(f"user-ratelimit-count-{user}")
     unique_ttl = tr.ttl(f"user-ratelimit-unique-{user}")
-
-    total = tr.incr(f"user-ratelimit-count-{user}")
-    is_unique = tr.sadd(f"user-ratelimit-unique-{user}", content)
-    total_unique = tr.scard(f"user-ratelimit-unique-{user}")
+    yield tr
     await tr.execute()
-    if await count_ttl == -2:
+    if await count_ttl <= 0:
         await redis.expire(f"user-ratelimit-count-{user}", RATELIMIT_TIME)
-    if await unique_ttl == -2:
+    if await unique_ttl <= 0:
         await redis.expire(f"user-ratelimit-unique-{user}", RATELIMIT_TIME)
+
+
+async def total_and_unique_messages(redis: Redis, user: int, content: str) -> Tuple[int, int, bool]:
+    async with _ttl(redis, user) as tr:
+        total = tr.incr(f"user-ratelimit-count-{user}")
+        is_unique = tr.sadd(f"user-ratelimit-unique-{user}", content)
+        total_unique = tr.scard(f"user-ratelimit-unique-{user}")
     return await total, await total_unique, bool(await is_unique)
 
 
 async def delete_user_message(redis: Redis, user: int, content: str):
-    tr = redis.pipeline()
-    tr.decr(f"user-ratelimit-count-{user}")
-    tr.srem(f"user-ratelimit-unique-{user}", content)
-    await tr.execute()
+    async with _ttl(redis, user) as tr:
+        tr.decr(f"user-ratelimit-count-{user}")
+        tr.srem(f"user-ratelimit-unique-{user}", content)
 
 
 async def can_send_spam_dm(redis: Redis, user: int) -> bool:
