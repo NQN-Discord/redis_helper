9ccc3ec11ce8525d3113e3db75dbaab0c28ec506
Blue <blue@nqn.blue>
2021-03-29 19:06:43 +0100

    Add created_recently

--- redis_helper/pack.py
+++ redis_helper/pack.py
@@ -1,13 +1,18 @@
 from typing import Iterator
 from aioredis import Redis
 
+ONE_WEEK = 7 * 24 * 60 * 60
+
 
 async def fetch_all_ids(redis: Redis) -> Iterator[int]:
     return map(int, await redis.smembers("pack-guild-ids"))
 
 
 async def create(redis: Redis, guild_id: int):
-    await redis.sadd("pack-guild-ids", guild_id)
+    tr = redis.pipeline()
+    tr.sadd("pack-guild-ids", guild_id)
+    tr.set(f"pack-recent-{guild_id}", 0, expire=ONE_WEEK)
+    await tr.execute()
 
 
 async def delete(redis: Redis, *guild_ids: int):
@@ -17,4 +22,8 @@ async def delete(redis: Redis, *guild_ids: int):
 
 
 async def exists(redis: Redis, guild_id: int):
-    return await redis.sismember("pack-guild-ids", guild_id)
+    return bool(await redis.sismember("pack-guild-ids", guild_id))
+
+
+async def created_recently(redis: Redis, guild_id: int) -> bool:
+    return bool(await redis.exists(f"pack-recent-{guild_id}"))
