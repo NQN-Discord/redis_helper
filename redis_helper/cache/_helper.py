da27cca61af510d6a77596f2f039a095851403be
Blue <blue@nqn.blue>
2021-04-02 14:41:08 +0100

    Update logging permissions

--- redis_helper/cache/_helper.py
+++ redis_helper/cache/_helper.py
@@ -23,6 +23,7 @@ def parse_roles(tr, guild_id, roles):
         role.pop("permissions_new", None)
         role.pop("color", None)
         role.pop("hoist", None)
+        role["permissions"] = int(role["permissions"])
     return OptionallyAwaitable(
         tr.hmset_dict(f"roles-{guild_id}", {role["id"]: msgpack.packb(role) for role in roles})
     )
@@ -30,14 +31,14 @@ def parse_roles(tr, guild_id, roles):
 
 def parse_channels(tr, guild_id, channels):
     for channel in range(len(channels)-1, -1, -1):
-        if channels[channel]["type"] in (0, 1, 5):
+        if channels[channel]["type"] in (0, 5):
             channels[channel].pop("topic", None)
             channels[channel].pop("last_message_id", None)
             channels[channel].pop("last_pin_timestamp", None)
             channels[channel].pop("parent_id", None)
             for perm_override in channels[channel].get("permission_overwrites", []):
-                del perm_override["deny_new"]
-                del perm_override["allow_new"]
+                perm_override["deny"] = int(perm_override["deny"])
+                perm_override["allow"] = int(perm_override["allow"])
         else:
             del channels[channel]
 
