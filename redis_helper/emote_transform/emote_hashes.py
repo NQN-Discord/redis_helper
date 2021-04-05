e1095a94e87b92df6fe217edd0d56aae285d535d
Blue <blue@nqn.blue>
2021-04-05 15:42:20 +0100

    Add heavy hitters script

--- redis_helper/emote_transform/emote_hashes.py
+++ redis_helper/emote_transform/emote_hashes.py
@@ -1,18 +1,43 @@
-from typing import List, Optional, NoReturn, Iterable
-from aioredis import Redis
+from typing import List
+from aioredis import Redis, ReplyError
+from hashlib import sha1
+import datetime
 
-hitters_script = """
-local set = ARGV[1]
-local key = ARGV[3]
-local set_length = tonumber(ARGV[2])
 
-if redis.call('ZRANK', set, key) then
-    redis.call('ZINCRBY', set, 1.0, key)
-elseif redis.call('ZCARD', set) < set_length then
-    redis.call('ZADD', set, 1.0, key)
-else
-    local value = redis.call('ZRANGE', set,0,0, 'withscores')
-    redis.call('ZREM', set, value[1])
-    redis.call('ZADD', set, value[2] + 1.0, key)
+EMOJI_COUNT_PER_DAY = 200
+hitters_script = f"""
+local set = KEYS[1]
+local set_length = {EMOJI_COUNT_PER_DAY}
+
+for i, key in ipairs(ARGV) do
+    if redis.call('ZRANK', set, key) then
+        redis.call('ZINCRBY', set, 1.0, key)
+    elseif redis.call('ZCARD', set) < set_length then
+        redis.call('ZADD', set, 1.0, key)
+    else
+        local value = redis.call('ZRANGE', set,0,0, 'withscores')
+        redis.call('ZREM', set, value[1])
+        redis.call('ZADD', set, value[2] + 1.0, key)
+    end
 end
-"""
+""".encode()
+hitters_hash = sha1(hitters_script).hexdigest()
+
+
+async def add_hashes(redis: Redis, hashes: List[str]):
+    day_number = datetime.date.today().weekday()
+    await _call_function(redis, hitters_script, hitters_hash, keys=[f"emoji-heavy-hitters-{day_number}"], args=hashes)
+
+
+async def get_hashes(redis: Redis):
+    pipeline = redis.pipeline()
+    for day_number in range(7):
+        pipeline.zrevrange(f"emoji-heavy-hitters-{day_number}", 0, EMOJI_COUNT_PER_DAY, withscores=True)
+    print(await pipeline.execute())
+
+
+async def _call_function(redis, script, script_hash, **kwargs):
+    try:
+        await redis.evalsha(script_hash, **kwargs)
+    except ReplyError:
+        await redis.eval(script, **kwargs)
