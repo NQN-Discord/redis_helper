a8ad83de1a45a88bf02b6764abf0b0048550bc3d
Blue <blue@nqn.blue>
2021-04-05 11:23:30 +0100

    Clear out emote_hashes.py

--- redis_helper/emote_transform/emote_hashes.py
+++ redis_helper/emote_transform/emote_hashes.py
@@ -1,43 +1,18 @@
 from typing import List, Optional, NoReturn, Iterable
 from aioredis import Redis
 
+hitters_script = """
+local set = ARGV[1]
+local key = ARGV[3]
+local set_length = tonumber(ARGV[2])
 
-async def assign_hash_ids(redis: Redis, emote_hash: str, ids: List[str], times_used: Optional[int]) -> NoReturn:
-    if ids:
-        tr = redis.pipeline()
-        for id in ids:
-            tr.set(f"extra-emote-id-hashes-{id}", emote_hash)
-
-        if times_used is not None:
-            tr.set(f"extra-emote-times-used-{emote_hash}", times_used)
-        tr.sadd(f"extra-emote-updates", emote_hash)
-        await tr.execute()
-
-
-async def delete(redis: Redis, emote_hash: str, ids: List[str]) -> NoReturn:
-    tr = redis.pipeline()
-    for id in ids:
-        tr.delete(f"extra-emote-id-hashes-{id}")
-    tr.delete(f"extra-emote-times-used-{emote_hash}")
-    tr.srem(f"extra-emote-updates", emote_hash)
-    await tr.execute()
-
-
-async def fetch_emote_hash(redis: Redis, emote_id: int) -> Optional[str]:
-    return await redis.get(f"extra-emote-id-hashes-{emote_id}")
-
-
-async def assign_emote_hash(redis: Redis, emote_id: int, emote_hash: str) -> NoReturn:
-    await redis.set(f"extra-emote-id-hashes-{emote_id}", emote_hash)
-
-
-async def increment_ids(redis: Redis, emote_ids: Iterable[str]) -> NoReturn:
-    tr = redis.pipeline()
-    for id in emote_ids:
-        tr.get(f"extra-emote-id-hashes-{id}")
-    hashes = set(await tr.execute()) - {None}
-    if hashes:
-        tr = redis.pipeline()
-        for emote_hash in hashes:
-            tr.incr(f"extra-emote-times-used-{emote_hash}")
-        await tr.execute()
+if redis.call('ZRANK', set, key) then
+    redis.call('ZINCRBY', set, 1.0, key)
+elseif redis.call('ZCARD', set) < set_length then
+    redis.call('ZADD', set, 1.0, key)
+else
+    local value = redis.call('ZRANGE', set,0,0, 'withscores')
+    redis.call('ZREM', set, value[1])
+    redis.call('ZADD', set, value[2] + 1.0, key)
+end
+"""
