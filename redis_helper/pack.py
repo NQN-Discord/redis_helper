9bb82a94bbeb31188892895a611f2ce3c877d95a
Blue <blue@nqn.blue>
2021-09-25 15:44:58 +0100

    Implement pack autocomplete

--- redis_helper/pack.py
+++ redis_helper/pack.py
@@ -1,3 +1,4 @@
+from typing import List
 from aioredis import Redis
 
 ONE_WEEK = 7 * 24 * 60 * 60
@@ -12,6 +13,10 @@ async def create(redis: Redis, guild_id: int, name: str):
 
 async def delete(redis: Redis, name: str):
     await redis.zrem("pack-guild-names", name)
+
+
+async def fetch_autocomplete(redis: Redis, start: str, limit: int) -> List[str]:
+    return await redis.zrangebylex("pack-guild-names", min=start.encode("utf8"), offset=0, count=limit)
 
 
 async def created_recently(redis: Redis, guild_id: int) -> bool:
