64a57ff78aefe41ec6ef27f09ed45f436d21edaa
Blue <blue@nqn.blue>
2021-10-28 22:39:32 +0100

    Fix autocomplete case insensitivity

--- redis_helper/pack.py
+++ redis_helper/pack.py
@@ -7,7 +7,7 @@ ONE_MINUTE = 60
 
 async def create(redis: Redis, guild_id: int, name: str):
     tr = redis.multi_exec()
-    tr.zadd("pack-guild-names", 0, name)
+    tr.zadd("pack-guild-names", 0, f"{name.lower()}{name}")
     tr.set(f"pack-recent-{guild_id}", 0, expire=ONE_WEEK)
     await tr.execute()
 
@@ -17,27 +17,28 @@ async def delete(redis: Redis, name: str):
 
 
 async def fetch_autocomplete(redis: Redis, start: str, limit: int) -> List[str]:
-    return await redis.zrangebylex("pack-guild-names", min=start.encode("utf8"), offset=0, count=limit)
+    packs = await redis.zrangebylex("pack-guild-names", min=start.lower().encode("utf8"), offset=0, count=limit)
+    return [p[len(p) // 2:] for p in packs]
 
 
 async def assign_user_packs_autocomplete(redis: Redis, user_id: int, packs: List[str]):
     tr = redis.multi_exec()
-    for pack in packs:
-        tr.zadd(f"pack-guild-names-{user_id}", 0, pack)
+    for name in packs:
+        tr.zadd(f"pack-guild-names-{user_id}", 0, f"{name.lower()}{name}")
     tr.expire(f"pack-guild-names-{user_id}", ONE_MINUTE)
     await tr.execute()
 
 
 async def fetch_user_packs_autocomplete(redis: Redis, user_id: int, start: str, limit: int) -> Tuple[List[str], bool]:
     tr = redis.multi_exec()
-    packs = tr.zrangebylex(f"pack-guild-names-{user_id}", min=start.encode("utf8"), offset=0, count=limit)
+    packs = tr.zrangebylex(f"pack-guild-names-{user_id}", min=start.lower().encode("utf8"), offset=0, count=limit)
     exists = tr.exists(f"pack-guild-names-{user_id}")
     await tr.execute()
-    return await packs, bool(await exists)
+    return [p[len(p) // 2:] for p in await packs], bool(await exists)
 
 
-async def delete_user_pack_autocomplete(redis: Redis, user_id: int, pack: str):
-    await redis.zrem(f"pack-guild-names-{user_id}", pack)
+async def delete_user_pack_autocomplete(redis: Redis, user_id: int, name: str):
+    await redis.zrem(f"pack-guild-names-{user_id}", f"{name.lower()}{name}")
 
 
 async def created_recently(redis: Redis, guild_id: int) -> bool:
