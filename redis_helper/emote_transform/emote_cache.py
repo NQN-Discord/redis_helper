54b2677de0750a9cfc17e8985437ac555ee4ca33
Blue <blue@nqn.blue>
2022-06-23 20:00:48 +0100

    Add sorted sets ordered by time

--- redis_helper/emote_transform/emote_cache.py
+++ redis_helper/emote_transform/emote_cache.py
@@ -1,5 +1,7 @@
 from typing import Dict, NoReturn, Optional
 from aioredis import Redis
+from time import monotonic_ns
+from itertools import repeat
 
 
 ONE_DAY = 24 * 60 * 60
@@ -9,9 +11,15 @@ async def assign(redis: Redis, guild_id: int, emotes: Dict[str, str]) -> NoRetur
     if emotes:
         tr = redis.pipeline()
         hash_key = f"emote-cache-{guild_id}"
+        sorted_set_key = f"emote-cache-{guild_id}-lookup"
+        current_time = monotonic_ns()
         tr.hmset_dict(hash_key, emotes)
+        tr.zadd(sorted_set_key, *[x for xs in zip(repeat(current_time), emotes.keys()) for x in xs])
+        await tr.execute()
+        tr = redis.pipeline()
         for key in emotes:
             tr.__getattr__("execute")(b"expiremember", hash_key, key, ONE_DAY)
+            tr.__getattr__("execute")(b"expiremember", sorted_set_key, key, ONE_DAY)
         await tr.execute()
 
 
