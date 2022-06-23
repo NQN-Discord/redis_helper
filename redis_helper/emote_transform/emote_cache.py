5e4cd51ef3918aa353bb4342c3a624e290615393
Blue <blue@nqn.blue>
2022-06-23 18:47:25 +0100

    Migrate LRU guild emote cache to hash and use keydb for per-key expires

--- redis_helper/emote_transform/emote_cache.py
+++ redis_helper/emote_transform/emote_cache.py
@@ -8,10 +8,12 @@ ONE_DAY = 24 * 60 * 60
 async def assign(redis: Redis, guild_id: int, emotes: Dict[str, str]) -> NoReturn:
     if emotes:
         tr = redis.pipeline()
-        for name, emote in emotes.items():
-            tr.set(f"emote-cache-{guild_id}-{name}", emote, expire=ONE_DAY)
+        hash_key = f"emote-cache-{guild_id}"
+        tr.hmset_dict(hash_key, emotes)
+        for key in emotes:
+            tr.__getattr__("execute")(b"expiremember", hash_key, key, ONE_DAY)
         await tr.execute()
 
 
 async def fetch(redis: Redis, guild_id: int, name: str) -> Optional[str]:
-    return await redis.get(f"emote-cache-{guild_id}-{name}")
+    return await redis.hget(f"emote-cache-{guild_id}", name)
