1712d72e36d186bc9c58c93b8fda62e65c2c04b6
Blue <blue@nqn.blue>
2022-06-29 12:24:12 +0100

    Add `fetch_all_by_score` for least recently used emojis

--- redis_helper/emote_transform/emote_cache.py
+++ redis_helper/emote_transform/emote_cache.py
@@ -1,4 +1,4 @@
-from typing import Dict, NoReturn, Optional
+from typing import Dict, NoReturn, Optional, List
 from aioredis import Redis
 from time import time_ns
 from itertools import repeat
@@ -18,10 +18,23 @@ async def assign(redis: Redis, guild_id: int, emotes: Dict[str, str]) -> NoRetur
         await tr.execute()
         tr = redis.pipeline()
         for key in emotes:
-            tr.__getattr__("execute")(b"expiremember", hash_key, key, ONE_DAY)
-            tr.__getattr__("execute")(b"expiremember", sorted_set_key, key, ONE_DAY)
+            _execute(tr, b"expiremember", hash_key, key, ONE_DAY)
+            _execute(tr, b"expiremember", sorted_set_key, key, ONE_DAY)
         await tr.execute()
 
 
 async def fetch(redis: Redis, guild_id: int, name: str) -> Optional[str]:
     return await redis.hget(f"emote-cache-{guild_id}", name)
+
+
+async def fetch_all_by_score(redis: Redis, guild_id: int, prefix: str) -> List[str]:
+    if prefix:
+        emotes = await redis.zrevrangebyscore(f"autocomplete-emojis-{guild_id}-recent")
+        emotes = (e for e in emotes if e.startswith(prefix))
+    else:
+        emotes = iter(await redis.zrevrangebyscore(f"autocomplete-emojis-{guild_id}-recent", offset=0, count=25))
+    return [e.split(".", 1)[1] for e in emotes]
+
+
+def _execute(redis, *args):
+    return redis.__getattr__("execute")(*args)
