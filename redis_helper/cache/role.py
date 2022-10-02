dcc7214757b848b079c0c0d9d405e8a014ca6b7c
Blue <blue@nqn.blue>
2022-10-02 14:34:28 +0100

    Fix setting roles

--- redis_helper/cache/role.py
+++ redis_helper/cache/role.py
@@ -3,7 +3,7 @@ from ._helper import parse_roles
 
 
 async def assign(redis: Redis, guild_id: int, role):
-    await parse_roles(redis, guild_id, [role])
+    await parse_roles(redis.hmset_dict, guild_id, [role])
 
 
 async def delete(redis: Redis, guild_id: int, role_id: int):
