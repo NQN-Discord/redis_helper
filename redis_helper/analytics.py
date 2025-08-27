fbf6efe2ad890c1510d24bd8c353ac6f22778887
Blue <blue@nqn.blue>
2025-08-27 20:30:37 +0100

    Add sentry span information

--- redis_helper/analytics.py
+++ redis_helper/analytics.py
@@ -1,9 +1,9 @@
 from aioredis import Redis
-from sentry_sdk import add_breadcrumb
+from sentry_sdk import add_breadcrumb, start_span
 
 
 def add_analytics(name: str, redis: Redis):
-    def execute(self, command, *args, **kwargs):
+    async def execute(self, command, *args, **kwargs):
         add_breadcrumb(
             message=f"Requested from {name} Redis",
             category="redis",
@@ -13,5 +13,9 @@ def add_analytics(name: str, redis: Redis):
             }
         )
 
-        return self._pool_or_conn.execute(command, *args, **kwargs)
+        with start_span(
+            op="redis",
+            name=f"{command.decode('utf-8')} {args}",
+        ):
+            return await self._pool_or_conn.execute(command, *args, **kwargs)
     redis.execute = execute.__get__(redis, type(redis))
